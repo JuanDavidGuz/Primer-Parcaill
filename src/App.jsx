c3cbec61320e3494265b975980a61877fed20356
Juan David Guzman A <juan_dav.guzman@uao.edu.co>
2023-03-08 16:43:09 -0500

    Entrega 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
+    const [Total,setTotal]= useState(0)
   
-  const [categories, setCategories] = useState(['2234  200000', '2345 20500'])
+  const [categories, setCategories] = useState([])
 
   const [text, setCategory] = useState( '' )
   const[valor, setvalor]= useState('')
 
   const addCategory = () => {
-      setCategories( [...categories, [text,- valor]])
-      setCategory('')
-      setvalor('')
+    if(text.length >= 3 && valor != '' ){
+        setCategories( [...categories, ["Numero: " + text, " - "+"Valor:  "+ valor]])
+        setCategory('')
+        setvalor('')
+        setTotal(Total+parseInt(valor))
+    }else{
+        alert('Ingrese un Numero de 3 o 4 Digutos')
+    }
   }
+  function LimiteMax(event) {
+    const maximo = 4;
+    if (event.target.value.length >= maximo && event.key !== 'Backspace') {
+      event.preventDefault();
+    }
+
+
+}
+ 
+
 
   return (
     <>
-    <h1>Bienvenido a su Gane</h1>
+    <h1 class="t">Bienvenido a su Gane</h1>
     <p>Ingrese el numero que va a juagar debe tener entre 3-4 diguitos:</p>
-    <input value={text} type="number" onChange={ e => setCategory(e.target.value) } />
+    <input value={text} type="number" onKeyDown={LimiteMax} onChange={ e => setCategory(e.target.value) } />
     <p>Ingrese el valor del chance:</p>
     <input value={valor} type="number" onChange={ e => setvalor(e.target.value) } />
     <br></br>
-    <button onClick={() => addCategory()}>Añadir</button>
+    <br></br>
+    <button onClick={() => addCategory()} class="ov-btn-slide-right" >Añadir</button>
 
     <ol>
         {
@@ -33,6 +51,8 @@ function App() {
             })
         }
     </ol>
+
+    <h2>Total a pagar:{Total} </h2>
 </>
   )
 }
